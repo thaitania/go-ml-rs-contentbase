d34ce4c23342d1872eb130fda89c331f930ab20f
Sittipong Jungsakul <sittipongwork@gmail.com>
2020-03-17 19:34:47 +0700

    add store categories and store item

--- example/movie_store/main.go
+++ example/movie_store/main.go
@@ -1,10 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	contentbase "github.com/thaitania/go-ml-rs-contentbase"
+)
+
 func main() {
 	println("=========== Example Movie Store =============")
 
-	// Init Ram store list of Item Data
-	// contentbase.InitItemList([]ItemProfile)
+	// Init Categories
+	cl := contentbase.InitItemCategories()
+	cl.NewCategories("cat_1", "Action")
+	cl.NewCategories("cat_2", "Adventure")
+	cl.NewCategories("cat_3", "Drama")
+	cl.NewCategories("cat_4", "Horror")
+	cl.NewCategories("cat_5", "Sci-fi")
+	cl.NewCategories("cat_6", "War")
+	cl.NewCategories("cat_7", "Western")
+	println("Categories List:")
+	println(fmt.Sprintf("%v", cl))
 
-	// Add User
+	// Init Item Profile
+	itp := contentbase.InitItemProfiles()
+	itp.NewItemProfile("m1", "movie_1", []string{"Action", "Adventure", "Western"})
+	itp.NewItemProfile("m2", "movie_2", []string{"Drama"})
+	itp.NewItemProfile("m3", "movie_3", []string{"Horror", "Sci-fi"})
+	itp.NewItemProfile("m4", "movie_4", []string{"Action", "Adventure", "Sci-fi", "War"})
+
+	// Create table Item and Categories
+	itp.ItemAttributeValue(cl)
+
+	// Init User Profile
+	// upf.UserItemRating()
 }
