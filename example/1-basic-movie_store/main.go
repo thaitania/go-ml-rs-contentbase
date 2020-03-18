1101cc38fadf1b01fb7ccf01281861c99bc1a9fd
Sittipong Jungsakul <sittipongwork@gmail.com>
2020-03-18 12:33:36 +0700

    calculate frequency score by using see item in categories frequency

--- example/1-basic-movie_store/main.go
+++ example/1-basic-movie_store/main.go
@@ -10,8 +10,8 @@ func main() {
 	println("=========== Example Movie Store =============")
 
 	// Init Categories
-	// if true: when you add new ItemProfile and categories doesn't exist, system will add categories to the categories list
-	// if false: system will add categories by function NewCategories("id", "title") only!
+	// If passing function with: true: when you add new ItemProfile and categories doesn't exist, system will add categories to the categories list
+	// If passing function with: false: system will only add categories by function NewCategories("id", "title")
 	cl := contentbase.InitItemCategories(true)
 	cl.NewCategories("Action")
 	cl.NewCategories("Adventure")
@@ -19,14 +19,13 @@ func main() {
 	cl.NewCategories("Horror")
 	cl.NewCategories("War")
 	cl.NewCategories("Western")
-	// println("Categories List:", len(cl.Categories))
-	// println(fmt.Sprintf("%v", cl))
 
 	// If you need to check duplicate category, just check error by err := cl.NewCategories("action", "Action")
 	// err := cl.NewCategories("action", "Action")
 	// if err != nil {
 	// 	panic(err)
 	// }
+
 	// Init Item Profile
 	itp := contentbase.InitItemProfiles()
 	itp.NewItemProfile(cl, "m1", "movie_1", []string{"Action"})
@@ -36,7 +35,7 @@ func main() {
 	itp.NewItemProfile(cl, "m3", "movie_3", []string{"Horror", "Sci-fi"})
 	itp.NewItemProfile(cl, "m4", "movie_4", []string{"Action", "Adventure", "Sci-fi", "War"})
 	itp.NewItemProfile(cl, "m5", "movie_5", []string{"Sci-fi", "Adventure"})
-	itp.NewItemProfile(cl, "m6", "movie_6", []string{"Sci-fi", "Horror"})
+	itp.NewItemProfile(cl, "m6", "movie_6", []string{"Action", "Sci-fi", "Horror"})
 
 	// Create table Item and Categories
 	iav, err := itp.ItemAttributeValue(cl)
@@ -50,7 +49,16 @@ func main() {
 	}
 
 	// Init User Profile
-	// upf.UserItemRating()
+	upf := contentbase.InitUserProfile()
+	upf.NewUserProfile("user1", "m1", 3)
+	upf.NewUserProfile("user1", "m6", 4)
+
+	upf.NewUserProfile("user2", "m2", 5)
+	upf.NewUserProfile("user2", "m4", 1)
+	upf.NewUserProfile("user2", "m5", 1)
+	// println(fmt.Sprintf("%v", upf))
 
 	// Predict item by using UserProfile frequency
+	println("============== GetScoreByCategoriesFrequency ==============")
+	contentbase.GetScoreByCategoriesFrequency(upf, iav)
 }
--- user-profile.go
+++ user-profile.go
@@ -1,33 +1,92 @@
 package contentbase
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
 
 // UserList is struct for contain user profile data list
 type UserList struct {
-	UserData map[string][]UserProfile
+	UserData map[string][]UserProfileItem
 }
 
-// UserProfile is struct for contain user profile usage data
-type UserProfile struct {
+// UserProfileItem is struct for contain user profile usage data
+type UserProfileItem struct {
 	ItemID string
 	Rating int
 }
 
-// InitUserProfile is function for init UserProfile memory
+// UserProfilePredictionCategories is struct for contain user prediction score
+type UserProfilePredictionCategories struct {
+	UserData map[string][]UserProfilePredictionCategoriesData
+}
+
+// UserProfilePredictionCategoriesData is struct for contain user prediction score Data
+type UserProfilePredictionCategoriesData struct {
+	CategoriesID string
+	Score        float32
+}
+
+// InitUserProfile is function for init UserProfileItem memory
 func InitUserProfile() *UserList {
 	return &UserList{
-		UserData: make(map[string][]UserProfile),
+		UserData: make(map[string][]UserProfileItem),
 	}
 }
 
 // NewUserProfile is function for add new item in list
 func (ul *UserList) NewUserProfile(userID string, itemID string, rating int) error {
 	if userID == "" {
-		return errors.New("UserProfile must have ID of user")
+		return errors.New("UserProfile must have 'ID' of user")
+	}
+	if itemID == "" {
+		return errors.New("UserProfile must have 'ItemID' of the item")
+	}
+
+	if ul.UserData[userID] != nil {
+		ul.UserData[userID] = append(ul.UserData[userID], UserProfileItem{ItemID: itemID, Rating: rating})
+	} else {
+		ul.UserData[userID] = []UserProfileItem{UserProfileItem{ItemID: itemID, Rating: rating}}
 	}
 	// ul.UserProfile[userID] = []UserProfile{
 	// 	ItemID: itemID,
 	// 	Rating: Rating,
 	// }
 	return nil
+}
+
+// GetScoreByCategoriesFrequency is function for predict by frequency only (predict without rating)
+func GetScoreByCategoriesFrequency(upf *UserList, iav *ItemAttributeValueData) (*UserProfilePredictionCategories, error) {
+	upps := &UserProfilePredictionCategories{
+		UserData: make(map[string][]UserProfilePredictionCategoriesData),
+	}
+	pdfq := make(map[string][]float32)
+	for k, v := range upf.UserData {
+		tmp := make([]float32, len(iav.Categories))
+		for _, vv := range v {
+			for k, vvv := range iav.Value[vv.ItemID] {
+				tmp[k] = tmp[k] + float32(vvv)
+			}
+			// println(k+"."+vv.ItemID, fmt.Sprintf("%v", iav.Value[vv.ItemID]), "rating="+strconv.Itoa(vv.Rating))
+		}
+		pdfq[k] = tmp
+	}
+
+	for k, v := range pdfq {
+		// println(k, fmt.Sprintf("%v", v))
+		for kk, vv := range v {
+			v[kk] = vv / float32(len(upf.UserData[k]))
+
+			upps.UserData[k] = append(upps.UserData[k], UserProfilePredictionCategoriesData{
+				CategoriesID: iav.Categories[kk],
+				Score:        v[kk],
+			})
+		}
+		sort.Slice(upps.UserData[k], func(i, j int) bool {
+			return upps.UserData[k][i].Score > upps.UserData[k][j].Score
+		})
+		println("Result=", k, fmt.Sprintf("%v", upps.UserData[k]))
+	}
+	return upps, nil
 }
